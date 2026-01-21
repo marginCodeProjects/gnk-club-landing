540e909f67dadb6918decb1c761a8277be378486
Vlad <fominvladosik@gmail.com>
2026-01-21 14:43:49 +0300

    adaptive main content

--- src/components/main/MoneyRisks.tsx
+++ src/components/main/MoneyRisks.tsx
@@ -6,7 +6,12 @@ import Image from "next/image";
 
 const MoneyRisks = () => {
     return (
-        <div className="my-20 rounded-3xl bg-blue-gradient text-white grid grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-1 gap-7 items-stretch">
+        <div className="
+            my-20 rounded-3xl overflow-hidden
+            bg-blue-gradient text-white grid
+            grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-1
+            gap-7 items-stretch"
+        >
             <div className="flex flex-col gap-7 mt-20 mx-7 md:my-10 md:ml-7">
                 <div className="text-4xl">Где чаще всего теряют деньги</div>
                 <div className="text-base md:text-xl">
